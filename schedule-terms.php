1404f1dc913764133852873ed4161fd98aff04da
Hiroshi Urabe <urabe@ham.works>
2022-05-13 22:08:10 +0900

    Release 0.1.1

--- schedule-terms.php
+++ schedule-terms.php
@@ -9,7 +9,7 @@
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:     schedule-terms
  * Domain Path:     /languages
- * Version: 0.1.0
+ * Version: 0.1.1
  *
  * @package Schedule_Terms
  */
