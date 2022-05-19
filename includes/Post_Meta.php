d6d3906793b16ba0c388bd1329c8d709176de93a
Hiroshi Urabe <urabe@ham.works>
2022-05-20 05:29:02 +0900

    register meta on init action

--- includes/Post_Meta.php
+++ includes/Post_Meta.php
@@ -11,6 +11,12 @@ namespace HAMWORKS\WP\Schedule_Terms;
  * Register post meta.
  */
 class Post_Meta {
+	/**
+	 * Post meta key.
+	 *
+	 * @var string
+	 */
+	private $meta_key;
 
 	/**
 	 * Constructor.
@@ -18,6 +24,14 @@ class Post_Meta {
 	 * @param string $meta_key post meta key.
 	 */
 	public function __construct( string $meta_key ) {
+		$this->meta_key = $meta_key;
+		add_action( 'init', array( $this, 'init' ), 100 );
+	}
+
+	/**
+	 * Init.
+	 */
+	public function init() {
 		foreach ( get_post_types() as $post_type ) {
 			$schema = array(
 				'type'       => 'object',
@@ -41,7 +55,7 @@ class Post_Meta {
 
 			register_post_meta(
 				$post_type,
-				$meta_key,
+				$this->meta_key,
 				array(
 
 					'single'       => false,
--- includes/Term_Meta.php
+++ includes/Term_Meta.php
@@ -11,6 +11,12 @@ namespace HAMWORKS\WP\Schedule_Terms;
  *  Term Meta class.
  */
 class Term_Meta {
+	/**
+	 * Term meta key.
+	 *
+	 * @var string
+	 */
+	private $meta_key;
 
 	/**
 	 * The single instance of the class.
@@ -18,6 +24,23 @@ class Term_Meta {
 	 * @param string $meta_key The meta key.
 	 */
 	public function __construct( string $meta_key ) {
+		$this->meta_key = $meta_key;
+		add_action( 'init', array( $this, 'init' ), 100 );
+	}
+
+	/**
+	 * Init.
+	 */
+	public function init() {
+		$this->register_term_meta( $this->meta_key );
+	}
+
+	/**
+	 * Register Term meta.s
+	 *
+	 * @param string $meta_key The meta key.
+	 */
+	public function register_term_meta( string $meta_key ) {
 		foreach ( get_taxonomies() as $taxonomy ) {
 			register_term_meta(
 				$taxonomy,
