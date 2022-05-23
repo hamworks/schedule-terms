5059bdb7ee73ad61a42c3661fe677d7a2d8e7031
torounit <mail@torounit.com>
2022-05-24 05:19:32 +0900

    add test stub

---
+++ tests/Schedule_Test.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Test for Schedule class.
+ *
+ * @package Schedule_Terms
+ */
+
+namespace HAMWORKS\WP\Schedule_Terms\Tests;
+
+use HAMWORKS\WP\Schedule_Terms\Schedule;
+use \WP_UnitTestCase;
+
+/**
+ * Test Class.
+ */
+class Schedule_Test extends WP_UnitTestCase {
+
+	/**
+	 * Test for Schedule::is_expired().
+	 */
+	public function test_is_expired() {
+		$schedule = new Schedule(
+			array(
+				'type'     => 'attach',
+				'taxonomy' => 'category',
+				'term'     => 'category-1',
+				'datetime' => '2012-07-05T22:09:28+09:00',
+			)
+		);
+		$this->assertTrue( $schedule->is_expired( strtotime( '2020-07-05T22:09:28+09:00' ) ) );
+		$this->assertFalse( $schedule->is_expired( strtotime( '2000-07-05T22:09:28+09:00' ) ) );
+	}
+
+	/**
+	 * Test for Schedule::get_timestamp().
+	 */
+	public function test_get_timestamp() {
+		$schedule = new Schedule(
+			array(
+				'type'     => 'attach',
+				'taxonomy' => 'category',
+				'term'     => 'category-1',
+				'datetime' => '2012-07-05T22:09:28+09:00',
+			)
+		);
+
+		$this->assertEquals(
+			strtotime( '2012-07-05T22:09:28+09:00' ),
+			$schedule->get_timestamp()
+		);
+	}
+
+	/**
+	 * Test for Schedule::get_wp_term().
+	 */
+	public function test_get_wp_term() {
+		$schedule = new Schedule(
+			array(
+				'type'     => 'attach',
+				'taxonomy' => 'category',
+				'term'     => 'category-1',
+				'datetime' => '2012-07-05T22:09:28+09:00',
+			)
+		);
+
+		$this->assertNull( $schedule->get_wp_term() );
+
+		$this->factory()->category->create(
+			array(
+				'slug' => 'category-1',
+			)
+		);
+
+		$this->assertInstanceOf(
+			'WP_Term',
+			$schedule->get_wp_term()
+		);
+	}
+}
---
+++ tests/Term_Manager_Test.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Test for Term_Manager class.
+ *
+ * @package Schedule_Terms
+ */
+
+namespace HAMWORKS\WP\Schedule_Terms\Tests;
+
+use HAMWORKS\WP\Schedule_Terms\Term_Manager;
+use PHPUnit\Framework\TestCase;
+use WP_UnitTestCase;
+
+/**
+ * Test Class.
+ */
+class Term_Manager_Test extends WP_UnitTestCase {
+
+	/**
+	 * Test for Term_Manager::update_post_term_relations().
+	 */
+	public function test_update_post_term_relations() {
+		$this->assertEquals( 1, 1 );
+	}
+
+	/**
+	 * Test for Term_Manager::update_schedule().
+	 */
+	public function test_update_schedule() {
+		$this->assertEquals( 1, 1 );
+	}
+
+}
