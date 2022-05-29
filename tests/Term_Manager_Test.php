88b5cc1992687f5b6b4075b067862d900a177090
Hiroshi Urabe <urabe@ham.works>
2022-05-29 17:00:00 +0900

    test for update_post_term_relations

--- tests/Term_Manager_Test.php
+++ tests/Term_Manager_Test.php
@@ -20,7 +20,28 @@ class Term_Manager_Test extends WP_UnitTestCase {
 	 * Test for Term_Manager::update_post_term_relations().
 	 */
 	public function test_update_post_term_relations() {
-		$this->assertEquals( 1, 1 );
+		$term_manager = new Term_Manager( 'schedule_terms', 'schedule_terms_active' );
+
+		$category_id = $this->factory()->category->create( array( 'name' => 'category-1' ) );
+		update_term_meta( $category_id, 'schedule_terms_active', true );
+
+		$post_id = $this->factory()->post->create();
+		add_post_meta(
+			$post_id,
+			'schedule_terms',
+			array(
+				'type'     => 'attach',
+				'taxonomy' => 'category',
+				'term'     => 'category-1',
+				'datetime' => '2012-07-05T22:09:28+09:00',
+			),
+			false
+		);
+
+
+		$term_manager->update_post_term_relations( $post_id );
+
+		$this->assertTrue( has_category( 'category-1', $post_id ) );
 	}
 
 	/**
